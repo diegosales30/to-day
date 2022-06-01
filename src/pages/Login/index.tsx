f2c015b37c90f8ebf4c07134ce4d74dcb81b6c4f
diegosales30 <diegofelipesales23@gmail.com>
2022-06-01 01:51:37 -0400

    finalizando resposividade da pagina de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -44,35 +44,47 @@ export const Login = () => {
   return (
     <Flex
       alignItems="center"
-      padding="10px 15px"
-      height="100vh"
-      bgGradient="linear(to-r, purple.800 65%, white 35%)"
+      justifyContent="center"
+      padding={["10px 15px", "10px 15px", "0px", "0px"]}
+      height={["auto", "auto", "100vh", "100vh"]}
+      bgGradient={[
+        "linear(to-b, purple.800 65%, white 35%)",
+        "linear(to-b, purple.800 65%, white 35%)",
+        "linear(to-r, purple.800 65%, white 35%)",
+        "linear(to-r, purple.800 65%, white 35%)",
+      ]}
       color="white"
     >
       <Flex
-        w="100%"
+        w={["100%", "100%", "90%", "90%"]}
         justifyContent="center"
-        flexDirection="row"
+        flexDirection={["column", "column", "row", "row"]}
         alignItems="center"
       >
-        <Grid w="100%" paddingRight="100px">
-          <Image src={logobranco} />
-          <Heading as="h1">O jeito fácil e gratis</Heading>
-          <Text>
+        <Grid w={["100%", "100%", "50%", "50%"]} paddingRight="100px">
+          <Image
+            src={logobranco}
+            alt="doit"
+            boxSize={["120px", "120px", "150px", "150px"]}
+          />
+          <Heading mt="4" as="h1">
+            O jeito fácil e gratis
+          </Heading>
+          <Text maxW="350px">
             Flexível e atrativo de gerenciar
-            <b>seus projetos em uma única plataforma</b>
+            <b> seus projetos em uma única plataforma</b>
           </Text>
         </Grid>
         <Grid
           onSubmit={handleSubmit(handleSignIn)}
           as="form"
-          mt="4"
-          w="50%"
           padding="30px 15px"
           border="3px solid"
           borderColor="gray.100"
           bg="white"
           color="grey.900"
+          mt={["4", "4", "0", "0"]}
+          w={["100%", "100%", "40%", "40%"]}
         >
           <Heading size="lg">Bem vindo de volta</Heading>
           <VStack mt="6" spacing="5">
